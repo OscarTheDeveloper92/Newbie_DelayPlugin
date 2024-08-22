490bf1e8f7be25c3b34f7b23aed115a51e6238e3
Oscar Rodriguez <oscarthedeveloper@outlook.com>
2024-08-22 00:13:12 -0700

    Feedback level update to -100% to 100%
    Updated the feedback parameter to be able to be adjusted from -100% to 100%. The knob begins at noon at 0% and can go in either direction. Turning the dial left will bring the parameter to negative values and dialing the knob will give it a positive value.

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -95,7 +95,7 @@ Parameters::createParameterLayout()     // also added a range from -12db to 12db
     layout.add(std::make_unique<juce::AudioParameterFloat>(
         feedbackParamID,
         "Feedback",
-        juce::NormalisableRange<float>(0.0f, 100.0f, 1.0f),
+        juce::NormalisableRange<float>(-100.0f, 100.0f, 1.0f),
         0.0f, //Important this is ZERO!!!
         juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromPercent)));
 
